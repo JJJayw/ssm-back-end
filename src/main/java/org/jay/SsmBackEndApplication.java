a2f1c9fb25cba26f79a3accb20abc64120540939
JJJayw <271753416@qq.com>
2024-04-12 14:53:30 +0800

    初始化项目

---
+++ src/main/java/org/jay/SsmBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.jay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmBackEndApplication.class, args);
+	}
+
+}
